f9aba6301560a1c0bdbba1e5884d046d55838f5f
Popescu Eduard <eduardpopescu199@gmail.com>
2023-08-31 19:11:21 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TodoForm from './TodoForm.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TodoForm />
+  </React.StrictMode>,
+)
